8f202458f1fd817c9f5b8d5c2f54fff9936c05d5
Theo <rebe83a@gmail.com>
2018-02-27 22:52:46 -0600

    fixed code to work with the program

--- src/Battler.java
+++ src/Battler.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
+import Data.*;
 
 public class Battler {
 
     private Player player;
-    private Monster monster;
-    private Room room;
+    private Animal animal;
+    private Location room;
     private boolean battling;
 
-    protected Battler(Player player, String monster) {
+    protected Battler(Player player, String animalName) {
         this.player = player;
-        this.monster = MonsterAdventure.layout.getMonster(monster);
-        this.room = player.currentRoom;
+        this.animal = room.getAnimal(animalName);
+        this.room = player.getCurrentLocation();
         battling = true;
     }
 
     public boolean battle() {
         Scanner scan = new Scanner(System.in);
         String input;
-        Double damage;
+        int damage;
         while(battling) {
-            System.out.println("You engage the " + monster.name + " in a duel!");
+            System.out.println("You engage the " + animal.getBreed().name() + " in a duel!");
             input = scan.nextLine();
 
             if(input.toLowerCase().regionMatches(0,"attack",0,6)) {
-                damage = player.attack - monster.defense;
+                damage = player.getAttack() - animal.getDefense();
                 if (input.toLowerCase().equals("attack with " )) {
-                    if (Item.getItem(input.substring(12), player.inventory) != null) {
-                        damage = player.attack + Item.getItem(input.substring(12), player.inventory).damage - monster.defense;
+                    if (Item.getItem(input.substring(12), player.getInventory()) != null) {
+                        damage = player.getAttack()
+                                + Item.getItem(input.substring(12), player.getInventory()).getAttack()
+                                - animal.getDefense();
                     }
                 }
-                monster.health = monster.health - damage;
-                if (monster.health < 0) {
+                animal.setHealth(animal.getHealth() - damage);
+                if (animal.getHealth() < 0) {
                     System.out.println("Congratulations! You have slain the monster.");
 
                 }
